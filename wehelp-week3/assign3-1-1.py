a893928bfb49850732e0ceadf9f717dd6e796ab0
yiling <a0970518217@gmail.com>
2026-06-28 02:33:20 +0800

    modify:修正csv格式錯誤
    - 原本csv格式遇到英文地址未使用括號，造成格式錯誤
    改為使用套件處理

--- wehelp-week3/assign3-1-1.py
+++ wehelp-week3/assign3-1-1.py
@@ -1,5 +1,6 @@
 import urllib.request  #負責抓網路資料
 import json           #負責處理json格式的資料
+import csv            #負責處理csv格式的資料
 
 url1="https://resources-wehelp-taiwan-b986132eca78c0b5eeb736fc03240c2ff8b7116.gitlab.io/hotels-ch"
 url2="https://resources-wehelp-taiwan-b986132eca78c0b5eeb736fc03240c2ff8b7116.gitlab.io/hotels-en"
@@ -13,14 +14,22 @@ data2=response2.read()  #讀取url2的資料
 data1=json.loads(data1)  #將json資料轉成 python dictionary
 data2=json.loads(data2)  #將json資料轉成 python dictionary
 
-file=open("hotels.csv","w",encoding="utf-8")
-file.write("ChineseName,EnglishName,ChineseAddress,EnglishAddress,Phone,RoomCount\n")
+file=open("hotels.csv","w",encoding="utf-8",newline="")
+writer=csv.writer(file)
+writer.writerow(["ChineseName","EnglishName","ChineseAddress","EnglishAddress","Phone","RoomCount"])
 
 for i in range(len(data1["list"])):   #會重複執行 len(data1["list"]) 次
     chinese_hotel = data1["list"][i]  #取得第 i 個中文hotel的資料
     english_hotel = data2["list"][i]  #取得第 i 個英文hotel的資料
     
-    file.write(chinese_hotel["旅宿名稱"]+","+english_hotel["hotel name"]+","+chinese_hotel["地址"]+","+english_hotel["address"]+","+chinese_hotel["電話或手機號碼"]+","+chinese_hotel["房間數"]+"\n")
+    writer.writerow([
+        chinese_hotel["旅宿名稱"],
+        english_hotel["hotel name"],
+        chinese_hotel["地址"],
+        english_hotel["address"],
+        chinese_hotel["電話或手機號碼"],
+        chinese_hotel["房間數"]
+    ])
     print(
         chinese_hotel["旅宿名稱"],          #印出中文hotel名稱
         english_hotel["hotel name"],        #印出英文hotel名稱
